bfb10e182de994947b12241c7716fb211f0c4ac3
Nithilan M <nithi1079@gmail.com>
2026-09-25 23:38:24 +0530

    declared tasks and habits as arraylists

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import model.Task;
+
+import java.util.ArrayList;
+
 import model.Habit;
 
 public class Main{
     public static void main(String[] args){
+        ArrayList<Task> tasks = new ArrayList<>();
+        ArrayList<Habit> habits = new ArrayList<>();
         Habit h1 = new Habit(
             "Solve DSA",
             1,
@@ -11,6 +16,7 @@ public class Main{
             "Solve 2 sliding window problems"
         );
 
+        habits.add(h1);
         h1.complete();
         h1.displayHabit();
 }
